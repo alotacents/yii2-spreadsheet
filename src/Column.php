a712adb4d4701e6d039aed9ec73215cc4d3bf429
alotacents <alotacents@gmail.com>
2023-08-30 16:32:46 -0700

    Update Column.php

--- src/Column.php
+++ src/Column.php
@@ -5,7 +5,7 @@
  * @license [New BSD License](http://www.opensource.org/licenses/bsd-license.php)
  */
 
-namespace alotacents\yii2-spreadsheet;
+namespace alotacents\yii2\spreadsheet;
 
 use Closure;
 use yii\base\BaseObject;
